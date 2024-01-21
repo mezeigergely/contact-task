5bc7b1f5b6609a5599a243ffe19b42bc356a2473
mezeigergely <mezei.gergely89@gmail.com>
2024-01-21 10:01:06 +0100

    added flash messages

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -9,24 +9,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Ticket;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class ContactController extends AbstractController
 {
-    /*#[Route('/contact', name: 'contact')]
-    public function index(): Response
-    {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-        ]);
-    }*/
-
-    const SUCCESS_MESSAGE = [
-        'title' => 'Köszönjük szépen a kérdésedet!',
-        'message' => 'Válaszunkkal hamarosan keresünk a megadott e-mail címen.'
-    ];
+    const SUCCESS_MESSAGE = 'Köszönjük szépen a kérdésedet! Válaszunkkal hamarosan keresünk a megadott e-mail címen.';
+    const ERROR_MESSAGE = 'Hiba! Kérjük töltsd ki az összes mezőt!';
     
     #[Route('/contact', name: 'contact')]
-    public function contact(Request $request, EntityManagerInterface $entityManager)
+    public function contact(Request $request, EntityManagerInterface $entityManager, SessionInterface $session)
     {
         $ticket = new Ticket();
         $form = $this->createForm(ContactFormType::class, $ticket);
@@ -35,21 +26,13 @@ class ContactController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($ticket);
             $entityManager->flush();
-
-            // Esetleg további logika vagy válasz küldése
-
-            return $this->redirectToRoute('success_page');
-        }
+            $session->getFlashBag()->add('success', $this::SUCCESS_MESSAGE);
+            
+            return $this->redirectToRoute('contact');
+        } 
 
         return $this->render('contact/contact.html.twig', [
             'form' => $form->createView(),
         ]);
     }
-
-    #[Route('/success', name: 'success_page')]
-    public function success(): Response
-    {
-        return $this->render('contact/success.html.twig',['message' => $this::SUCCESS_MESSAGE]);
-    }
-
 }
